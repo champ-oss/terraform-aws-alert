a52341f761f7c7c2b61f2a13cb278f9893b212f4
Phillip Langley <philliplangley@champmbplangley.lan>
2022-05-05 15:47:31 -0500

    baseline alert release

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -94,8 +94,8 @@ func TestExamplesCluster(t *testing.T) {
 
 		Vars: map[string]interface{}{
 			// We also can see how lists and maps translate between terratest and terraform.
-			"slack_url": slack_url,
-			"region":    region,
+			"SLACK_URL": slack_url,
+			"REGION":    region,
 		},
 
 		// Disable colors to parse stdout/stderr
