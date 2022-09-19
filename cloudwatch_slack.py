e91ab54e0dcfb82c6c9ce9b3882621fac0c288d5
Phillip Langley <philliplangley@champmbplangley.lan>
2022-09-19 09:52:52 -0500

    new ecr repos

--- cloudwatch_slack.py
+++ cloudwatch_slack.py
@@ -51,7 +51,7 @@ def lambda_handler(event, context):
             'short': False,
          }, {
             'title': 'Link to Error',
-            'title_link': "https://console.aws.amazon.com/cloudwatch/home?" + region + "#logsV2:log-groups/log-group/" + log_group_escaped + "/log-events/" + log_stream_escaped + "$3FfilterPattern$3D$26start$3D" + log_timestamp_start + "$26end$3D" + log_timestamp_endtime,
+            'value': "https://console.aws.amazon.com/cloudwatch/home?" + region + "#logsV2:log-groups/log-group/" + log_group_escaped + "/log-events/" + log_stream_escaped + "$3FfilterPattern$3D$26start$3D" + log_timestamp_start + "$26end$3D" + log_timestamp_endtime,
             'short': False,
          }],
        }],
