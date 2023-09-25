27d1b5ded6933f30b4d7c23dec8fcaad6150daad
Justin Reeves <reeves122@gmail.com>
2023-09-25 09:43:44 -0500

    test creating log message

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,8 +1,18 @@
 data "aws_region" "this" {}
 
+variable "slack_url" {
+  description = "Slack URL for testing"
+  type        = string
+}
+
 resource "aws_cloudwatch_log_group" "this" {
   name              = "terraform-aws-alert/test"
   retention_in_days = 5
+}
+
+resource "aws_cloudwatch_log_stream" "this" {
+  name           = "Test"
+  log_group_name = aws_cloudwatch_log_group.this.name
 }
 
 module "this" {
@@ -10,11 +20,16 @@ module "this" {
   git            = "terraform-aws-alert"
   log_group_name = aws_cloudwatch_log_group.this.name
   name           = "terraform-aws-alert"
-  slack_url      = ""
+  slack_url      = var.slack_url
   region         = data.aws_region.this.name
 }
 
 output "cloudwatch_log_group" {
-  description = "alarm name output"
-  value       = module.this.cloudwatch_log_group
+  description = "log group name"
+  value       = aws_cloudwatch_log_group.this.name
+}
+
+output "cloudwatch_log_stream" {
+  description = "log stream name"
+  value       = aws_cloudwatch_log_stream.this.name
 }
---
+++ examples/complete/test.sh
@@ -0,0 +1,6 @@
+set -e
+
+aws logs put-log-events \
+--log-group-name $CLOUDWATCH_LOG_GROUP \
+--log-stream-name $CLOUDWATCH_LOG_STREAM \
+--log-events "timestamp=1433190184356,message=2022-03-27 03:33:17.998 ERROR 7 --- [           main] test"
