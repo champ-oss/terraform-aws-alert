50c0f88d73ff290af693b57d5fd8043c62a65c6f
Justin Reeves <reeves122@gmail.com>
2023-09-25 09:57:25 -0500

    remove brackets from test message

--- examples/complete/test.sh
+++ examples/complete/test.sh
@@ -3,4 +3,4 @@ set -e
 aws logs put-log-events \
 --log-group-name $CLOUDWATCH_LOG_GROUP \
 --log-stream-name $CLOUDWATCH_LOG_STREAM \
---log-events timestamp=1433190184356,message="2022-03-27 03:33:17.998 ERROR 7 --- [           main] test"
+--log-events timestamp=1433190184356,message="2022-03-27 03:33:17.998 ERROR 7 --- main test"
