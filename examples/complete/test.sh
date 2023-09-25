6ea361f832753b59967d1972f21ea0475dbaf2f7
Justin Reeves <reeves122@gmail.com>
2023-09-25 10:46:22 -0500

    get current epoch time for test log message

--- examples/complete/test.sh
+++ examples/complete/test.sh
@@ -3,7 +3,7 @@ set -e
 aws logs put-log-events \
 --log-group-name $CLOUDWATCH_LOG_GROUP \
 --log-stream-name $CLOUDWATCH_LOG_STREAM \
---log-events timestamp=1433190184356,message="2022-03-27 03:33:17.998 ERROR 7 --- main test"
+--log-events timestamp=`date +%s`,message="2022-03-27 03:33:17.998 ERROR 7 --- main test"
 
 sleep 30
 
