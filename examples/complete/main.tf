adca3fabcaaaab2f0890e8d260ac34344b2ffb5c
Justin Reeves <reeves122@gmail.com>
2023-09-25 09:29:48 -0500

    update workflows

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,26 +1,20 @@
-locals {
-  tags = {
-    cost    = "shared"
-    creator = "terraform"
-    git     = var.git
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
+data "aws_region" "this" {}
 
 resource "aws_cloudwatch_log_group" "this" {
-  name              = "${var.git}/test"
+  name              = "terraform-aws-alert/test"
   retention_in_days = 5
-  tags              = local.tags
 }
 
 module "this" {
   source         = "../../"
-  git            = var.git
+  git            = "terraform-aws-alert"
   log_group_name = aws_cloudwatch_log_group.this.name
   name           = "terraform-aws-alert"
-  slack_url      = var.slack_url
-  region         = var.region
+  slack_url      = ""
+  region         = data.aws_region.this.name
+}
+
+output "cloudwatch_log_group" {
+  description = "alarm name output"
+  value       = module.this.cloudwatch_log_group
 }
--- examples/complete/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "cloudwatch_log_group" {
-  description = "alarm name output"
-  value       = module.this.cloudwatch_log_group
-}
