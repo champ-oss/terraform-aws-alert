283e533e50eb31d6f7e5b5d94816639c456e2996
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-11-08 15:01:46 -0600

    adding link to monitoring account

--- cloudwatch_slack.py
+++ cloudwatch_slack.py
@@ -53,9 +53,11 @@ def lambda_handler(event, context):
             'title': 'Link to Error',
             'value': "https://console.aws.amazon.com/cloudwatch/home?" + region + "#logsV2:log-groups/log-group/" + log_group_escaped + "/log-events/" + log_stream_escaped + "$3FfilterPattern$3D$26start$3D" + log_timestamp_start + "$26end$3D" + log_timestamp_endtime,
             'short': False,
+         }, {
+            'title': 'Monitoring Account Link to Error',
+            'value': "https://console.aws.amazon.com/cloudwatch/home?" + region + "#logsV2:log-groups/log-group/arn$253Aaws$253Alogs$253A" + region + "$253A" + payload['owner'] + "$253Alog-group$253A" + log_group_escaped + "/log-events/" + log_stream_escaped + "$3FfilterPattern$3D$26start$3D" + log_timestamp_start + "$26end$3D" + log_timestamp_endtime,
+            'short': False
          }],
-         "title": "Monitoring Account Link",
-         "title_link": "https://console.aws.amazon.com/cloudwatch/home?" + region + "#logsV2:log-groups/log-group/arn$253Aaws$253Alogs$253A" + region + "$253A" + payload['owner'] + "$253Alog-group$253A" + log_group_escaped + "/log-events/" + log_stream_escaped + "$3FfilterPattern$3D$26start$3D" + log_timestamp_start + "$26end$3D" + log_timestamp_endtime,
        }],
        }
        encoded_msg = json.dumps(msg).encode('utf-8')
