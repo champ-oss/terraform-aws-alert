152b45fb366de79c11243b0235e3fd83d8564322
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-11-09 13:15:48 -0600

    testing block msg

--- cloudwatch_slack.py
+++ cloudwatch_slack.py
@@ -101,10 +101,8 @@ def lambda_handler(event, context):
 				    "text": "<https://console.aws.amazon.com/cloudwatch/home?" + region + "#logsV2:log-groups/log-group/arn$253Aaws$253Alogs$253A" + region + "$253A" + payload['owner'] + "$253Alog-group$253A" + log_group_escaped + "/log-events/" + log_stream_escaped + "$3FfilterPattern$3D$26start$3D" + log_timestamp_start + "$26end$3D" + log_timestamp_endtime|Link to Centralized Logging>
 			     }
 		    }
-
-       ],
-
-       },
+       ]
+      }
 
        encoded_msg = json.dumps(msg).encode('utf-8')
        resp = http.request('POST',url, body=encoded_msg)
