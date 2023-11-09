72330b9677483c7216c4aa12a4d52c0444022452
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-11-09 12:00:27 -0600

    testing markdown language links

--- cloudwatch_slack.py
+++ cloudwatch_slack.py
@@ -50,12 +50,11 @@ def lambda_handler(event, context):
             'value': logEvent['message'],
             'short': False,
          }, {
-            'title': 'Link to Error',
-            'value': "https://console.aws.amazon.com/cloudwatch/home?" + region + "#logsV2:log-groups/log-group/" + log_group_escaped + "/log-events/" + log_stream_escaped + "$3FfilterPattern$3D$26start$3D" + log_timestamp_start + "$26end$3D" + log_timestamp_endtime,
-            'short': False,
+            "type": "mrkdwn",
+            "text": "<https://console.aws.amazon.com/cloudwatch/home?" + region + "#logsV2:log-groups/log-group/" + log_group_escaped + "/log-events/" + log_stream_escaped + "$3FfilterPattern$3D$26start$3D" + log_timestamp_start + "$26end$3D" + log_timestamp_endtime|Link to Error>"
          }, {
-            'title': 'Monitoring Account Link to Error',
-            'value': "https://console.aws.amazon.com/cloudwatch/home?" + region + "#logsV2:log-groups/log-group/arn$253Aaws$253Alogs$253A" + region + "$253A" + payload['owner'] + "$253Alog-group$253A" + log_group_escaped + "/log-events/" + log_stream_escaped + "$3FfilterPattern$3D$26start$3D" + log_timestamp_start + "$26end$3D" + log_timestamp_endtime,
+            "type": "mrkdwn",
+            'test': "<https://console.aws.amazon.com/cloudwatch/home?" + region + "#logsV2:log-groups/log-group/arn$253Aaws$253Alogs$253A" + region + "$253A" + payload['owner'] + "$253Alog-group$253A" + log_group_escaped + "/log-events/" + log_stream_escaped + "$3FfilterPattern$3D$26start$3D" + log_timestamp_start + "$26end$3D" + log_timestamp_endtime|Link to Centralized Logging,
             'short': False
          }],
        }],
