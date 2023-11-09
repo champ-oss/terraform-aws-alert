593191ad1a64959cff006b661db77ee040e6aa83
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-11-09 14:24:46 -0600

    testing block message with slack

--- cloudwatch_slack.py
+++ cloudwatch_slack.py
@@ -79,7 +79,7 @@ def lambda_handler(event, context):
                                         },
                                         {
                                             "type": "text",
-                                            "text": payload['logGroup'],
+                                            "text": payload['logGroup']
                                         }
                                     ]
                                 },
@@ -102,7 +102,7 @@ def lambda_handler(event, context):
                                         {
                                             "type": "link",
                                             "url": "https://console.aws.amazon.com/cloudwatch/home?" + region + "#logsV2:log-groups/log-group/" + log_group_escaped + "/log-events/" + log_stream_escaped + "$3FfilterPattern$3D$26start$3D" + log_timestamp_start + "$26end$3D" + log_timestamp_endtime,
-                                            "text": "Link to Error",
+                                            "text": "Link to Error"
                                         }
                                     ]
                                 },
@@ -112,7 +112,7 @@ def lambda_handler(event, context):
                                         {
                                             "type": "link",
                                             "url": "https://console.aws.amazon.com/cloudwatch/home?" + region + "#logsV2:log-groups/log-group/arn$253Aaws$253Alogs$253A" + region + "$253A" + payload['owner'] + "$253Alog-group$253A" + log_group_escaped + "/log-events/" + log_stream_escaped + "$3FfilterPattern$3D$26start$3D" + log_timestamp_start + "$26end$3D" + log_timestamp_endtime,
-                                            "text": "Link to Centralized Logging Error",
+                                            "text": "Link to Centralized Logging Error"
                                         }
                                     ]
                                 }
@@ -131,7 +131,7 @@ def lambda_handler(event, context):
                             "elements": [
                                 {
                                     "type": "text",
-                                    "text": "Log Error",
+                                    "text": "Log Error"
                                 }
                             ]
                         }
@@ -141,7 +141,7 @@ def lambda_handler(event, context):
                     "type": "section",
                     "text": {
                         "type": "plain_text",
-                        "text": logEvent['message'],
+                        "text": logEvent['message']
                     }
                 }
             ]
