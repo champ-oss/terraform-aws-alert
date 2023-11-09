45027198e326310042491bd01e73748241ae0a0f
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-11-09 13:56:02 -0600

    testing block message with slack

--- cloudwatch_slack.py
+++ cloudwatch_slack.py
@@ -112,7 +112,7 @@ def lambda_handler(event, context):
              							"elements": [
              								{
              									"type": "link",
-             									"url": "<https://console.aws.amazon.com/cloudwatch/home?" + region + "#logsV2:log-groups/log-group/arn$253Aaws$253Alogs$253A" + region + "$253A" + payload['owner'] + "$253Alog-group$253A" + log_group_escaped + "/log-events/" + log_stream_escaped + "$3FfilterPattern$3D$26start$3D" + log_timestamp_start + "$26end$3D" + log_timestamp_endtime,
+             									"url": "https://console.aws.amazon.com/cloudwatch/home?" + region + "#logsV2:log-groups/log-group/arn$253Aaws$253Alogs$253A" + region + "$253A" + payload['owner'] + "$253Alog-group$253A" + log_group_escaped + "/log-events/" + log_stream_escaped + "$3FfilterPattern$3D$26start$3D" + log_timestamp_start + "$26end$3D" + log_timestamp_endtime,
              									"text": "Link to Centralized Logging Error",
              									"style": {
              										"bold": true
@@ -148,7 +148,7 @@ def lambda_handler(event, context):
              			"type": "section",
              			"text": {
              				"type": "plain_text",
-             				"text": "long message is here you"
+             				"text": logEvent['message'],
              			}
              		}
              	]
