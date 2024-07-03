4414ba8e081e138afcc3cb1354148e20e74081c8
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-03 13:18:42 -0500

    toggle module

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -3,5 +3,5 @@ resource "aws_cloudwatch_log_subscription_filter" "this" {
   name            = "cloudwatch-to-slack-logfilter"
   log_group_name  = var.log_group_name
   filter_pattern  = var.filter_pattern
-  destination_arn = module.this.arn
+  destination_arn = module.this[0].arn
 }
