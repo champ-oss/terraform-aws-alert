1a36ef9bed7988dbe0bfe442547f1c0658b04b41
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-03 13:21:09 -0500

    toggle module

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "lambda_function_arn" {
   description = "function arn"
-  value       = var.module_enabled ? module.this.arn : ""
+  value       = var.module_enabled ? module.this[0].arn : ""
 }
 
 output "cloudwatch_log_group" {
   description = "alarm name output"
-  value       = var.module_enabled ? module.this.cloudwatch_log_group : ""
+  value       = var.module_enabled ? module.this[0].cloudwatch_log_group : ""
 }
