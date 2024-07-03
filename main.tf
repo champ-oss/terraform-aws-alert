b6a4c7992ced3af17071b1de9fa5370f9eaec686
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-03 15:45:52 -0500

    updating variable to enabled

--- main.tf
+++ main.tf
@@ -34,14 +34,15 @@ moved {
 }
 
 module "this" {
-  source           = "github.com/champ-oss/terraform-aws-lambda.git?ref=v1.0.142-273b055"
-  git              = var.git
-  name             = "${var.name}-${random_string.identifier[0].result}"
-  tags             = merge(local.tags, var.tags)
-  runtime          = "python3.8"
-  handler          = "cloudwatch_slack.lambda_handler"
-  filename         = data.archive_file.lambda_zip[0].output_path
-  source_code_hash = data.archive_file.lambda_zip[0].output_base64sha256
+  source                = "github.com/champ-oss/terraform-aws-lambda.git?ref=v1.0.142-273b055"
+  git                   = var.git
+  name                  = "${var.name}-${random_string.identifier[0].result}"
+  tags                  = merge(local.tags, var.tags)
+  runtime               = "python3.8"
+  handler               = "cloudwatch_slack.lambda_handler"
+  filename              = data.archive_file.lambda_zip[0].output_path
+  source_code_hash      = data.archive_file.lambda_zip[0].output_base64sha256
+  enable_logging_alerts = false
   environment = {
     SLACK_URL = var.slack_url
     REGION    = var.region
