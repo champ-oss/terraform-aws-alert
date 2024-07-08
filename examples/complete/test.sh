eafd156db4b472adbc77d339d9b4bb701ba0ad41
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-08 13:36:33 -0500

    testing with lambda resources

--- examples/complete/test.sh
+++ examples/complete/test.sh
@@ -6,7 +6,7 @@ set -e
     --log-group-name $CLOUDWATCH_LOG_GROUP \
     --log-stream-name $CLOUDWATCH_LOG_STREAM \
     --log-events timestamp=`date +%s%3N`,message="2022-03-27 03:33:17.998 ERROR 7 --- main test"
-    sleep 30
+    sleep 180
 
     aws logs tail $CLOUDWATCH_LOG_GROUP | grep -vi error
     aws logs tail $CLOUDWATCH_LOG_GROUP | grep -i "{'status_code': 200, 'response': 'ok'}"
