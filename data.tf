1588b9de7d7896f35e09d751e19865235a50a42d
Phillip Langley <philliplangley@champmbplangley.lan>
2024-10-09 09:11:52 -0500

    update archive to directory

--- data.tf
+++ data.tf
@@ -1,9 +1,8 @@
 data "archive_file" "lambda_zip" {
   count            = var.enabled ? 1 : 0
   type             = "zip"
-  output_file_mode = "0666"
   source_dir       = "${path.module}/alert"
-  output_path      = "${path.module}/cloudwatch_slack.zip"
+  output_path      = "${path.module}/alert/cloudwatch_slack.zip"
 }
 
 moved {
